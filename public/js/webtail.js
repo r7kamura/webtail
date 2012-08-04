9d4d975f4133ce3c86f960a118e98ff603914992
Ryo NAKAMURA <ryo-nakamura@cookpad.com>
2012-08-05 02:08:54 +0900

    Fix the execution sequence of onmessage callbacks

--- public/js/webtail.js
+++ public/js/webtail.js
@@ -1,30 +1,24 @@
 var Webtail = {
   run: function(port) {
     var self = this;
-
     jQuery(function($) {
-      var socket = new (WebSocket || MozWebSocket)('ws://localhost:' + port),
-          body   = $('body');
-
-      self.onmessages.push(function(message) {
-        // To ignore serial empty lines
-        if (message.data == '\n' && $('pre:last').text() == '\n') return;
-
-        // Insert a new line
-        $('<pre>').text(message.data).appendTo('body');
-
-        // Scroll to bottom of the page
-        $('html, body').scrollTop($(document).height());
-
-        // Trigger onmessage event
-        body.trigger('onmessage');
-      });
-
+      var socket = new (WebSocket || MozWebSocket)('ws://localhost:' + port);
       socket.onmessage = function(message) {
         $.each(self.onmessages, function() { this(message) });
       };
     });
   },
 
-  onmessages: []
+  onmessages: [
+    function(message) {
+      // To ignore serial empty lines
+      if (message.data == '\n' && $('pre:last').text() == '\n') return;
+
+      // Insert a new line
+      $('<pre>').text(message.data).appendTo('body');
+
+      // Scroll to bottom of the page
+      $('html, body').scrollTop($(document).height());
+    }
+  ]
 };
